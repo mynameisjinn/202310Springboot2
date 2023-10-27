583ffdcfdd3a936005185ccfa31e20a15762f1d6
hyejin <obndo8539@naver.com>
2023-10-27 15:37:58 +0900

    주문 할인 테스트 실행

---
+++ src/main/java/com/hello/core/OrderApp.java
@@ -0,0 +1,26 @@
+package com.hello.core;
+
+import com.hello.core.member.Grade;
+import com.hello.core.member.Member;
+import com.hello.core.member.MemberService;
+import com.hello.core.member.MemberServiceImpl;
+import com.hello.core.order.Order;
+import com.hello.core.order.OrderService;
+import com.hello.core.order.OrderServiceImpl;
+
+public class OrderApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        long memberId = 1L;
+        Member member = new Member(1L,"memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA",10000);
+
+        System.out.println("order = " + order);
+
+    }
+}
