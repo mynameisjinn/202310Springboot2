4d6b4a01d5fa009b7612741644897b79a432bfd2
hyejin <obndo8539@naver.com>
2023-11-06 14:00:30 +0900

    빈 초기화,종료 -  InitializingBean, DisposableBean

--- src/test/java/com/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/com/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -21,7 +21,7 @@ public class BeanLifeCycleTest {
         @Bean
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
-            networkClient.setUrl("http://hello-spring.dev"); // 수정자 주입
+            networkClient.setUrl("http://hello-spring.dev");
             return networkClient;
         }
     }
